7df2a7bc070cb75a09f793ec658ffb9931b0ab04
simonvannarath <vvidfinishing@gmail.com>
2017-08-22 13:17:36 +1000

    Classwork for 20170822 (OOP)

---
+++ Assets/~Breakout/Scripts/Ball.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Breakout
+{
+    public class Ball : MonoBehaviour
+    {
+        public float speed = 10f; // Speed the ball travels
+
+        private Vector3 velocity; // Velocity of the ball (direction x speed)
+
+	    // Use this for initialization
+	    void Start () {
+		}
+	
+	    // Update is called once per frame
+	    private void Update ()
+        {
+            // Moves ball using velocity & deltaTime
+            transform.position += velocity * Time.deltaTime;
+	    }
+
+        // Fires off ball in a given direction
+        public void Fire(Vector3 direction)
+        {
+            // Calculate velocity
+            velocity = direction * speed;
+        }
+        
+        // Detect collisions
+        void OnCollisionEnter2D(Collision2D other)
+        {
+            // Grab contact point of collision
+            ContactPoint2D contact = other.contacts[0];
+
+            // Calculate the reflection point of the ball using velocity & contact normal
+            Vector3 reflect = Vector3.Reflect(velocity, contact.normal);
+
+            // Calculate new velocity from reflection multiplied by the same speed (velocity.magnitude)
+            velocity = reflect.normalized * velocity.magnitude;
+
+        }
+
+    }
+}
+
+
---
+++ Assets/~Breakout/Scripts/Paddle.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Breakout
+{
+    public class Paddle : MonoBehaviour
+    {
+        public float movementSpeed = 20f;   // Speed the paddle moves
+        public Ball currentBall;            // Ball that should be attatched to the Paddle as as child
+        public Vector2[] directions = new Vector2[] // List of directions for the ball to choose from
+        {
+            new Vector2 (-0.5f, 0.5f),
+            new Vector2 (0.5f, 0.5f)
+        };
+
+	    // Use this for initialization
+	    void Start ()
+        {
+            // Grabs currentBall from children of the Paddle
+            currentBall = GetComponentInChildren<Ball>();
+	    }
+	
+	    // Update is called once per frame
+	    void Update ()
+        {
+            CheckInput();
+            Movement();
+	    }
+
+        void Fire()
+        {
+            // Detach as child
+            currentBall.transform.SetParent(null);
+
+            // Generate random dir from list of directions
+            Vector3 randomDir = directions[Random.Range(0, directions.Length)];
+
+            // Fire off ball in randomDirection
+            currentBall.Fire(randomDir);
+        }
+
+        void CheckInput ()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Fire();
+            }
+        }
+
+        void Movement()
+        {
+            // Get input on the horizontal axis
+            float inputH = Input.GetAxis("Horizontal");
+
+            // Set force to direction (inputH to decide which direction)
+            Vector3 force = transform.right * inputH;
+
+            // Apply movement speed to force
+            force *= movementSpeed;
+
+            // Apply deltaTime to force
+            force *= Time.deltaTime;
+
+            // Add force to position
+            transform.position += force;
+        }
+    }
+
+}

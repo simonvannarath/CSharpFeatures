72d395b9db2457a04d09704ef3dcd65431d632de
simonvannarath <vvidfinishing@gmail.com>
2017-08-31 23:54:29 +1000

    Classwork for 20170831 - Fixed lose condition check, added Tower Defense scene (need to replace teacher's pieces)

---
+++ Assets/~TowerDefense/Scripts/AIAgent.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.AI;
+
+namespace TownerDefense
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+
+    public class AIAgent : MonoBehaviour
+    {
+        public Transform target;
+
+        private NavMeshAgent nav;
+
+        private void Awake()
+        {
+            nav = GetComponent<NavMeshAgent>();
+            
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // If target is not null
+            if (target != null)
+            {
+                // Set nav destination to target's position
+                nav.SetDestination(target.position);
+            }
+        }
+    }
+
+}
